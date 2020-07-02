f0a1e6b680f405ad52fb8c0f802dfad25740f77d
King Elisha <ek.chibueze@gmail.com>
2020-07-02 14:45:31 +0100

    implemented biggerIsGreater

--- medium.js
+++ medium.js
@@ -37,4 +37,46 @@ function encryption(s) {
 
 // console.log(encryption("haveaniceday"));
 // console.log(encryption("feedthedog"));
-console.log(encryption("chillout"));
+// console.log(encryption("chillout"));
+
+/**
+ * Problem: https://www.hackerrank.com/challenges/bigger-is-greater/problem
+ * Explanation: https://www.nayuki.io/page/next-lexicographical-permutation-algorithm
+ *
+ * @param {string} w
+ */
+function biggerIsGreater(w) {
+  const arr = w.split("").map((s) => s.charCodeAt(0));
+
+  // find pivot
+  let current = 0;
+  let pivot = -1;
+  for (let i = arr.length - 1; i >= 0; i--) {
+    if (arr[i] < current) {
+      pivot = i;
+      break;
+    }
+    current = arr[i];
+  }
+
+  if (pivot < 0) return "no answer";
+
+  // swap
+  for (let i = arr.length - 1; i >= 0; i--) {
+    if (arr[i] > arr[pivot]) {
+      [arr[i], arr[pivot]] = [arr[pivot], arr[i]];
+      break;
+    }
+  }
+
+  // reverse arr from pivot
+  for (let i = pivot + 1, j = arr.length - 1; i < j; i++, j--) {
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+  }
+
+  return arr.map((n) => String.fromCharCode(n)).join("");
+}
+
+// console.log(biggerIsGreater("hefg"));
+// console.log(biggerIsGreater("ab"));
+console.log(biggerIsGreater("dkhc"));
